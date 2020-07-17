ebda09906ad6f3bc2b720be4d069bf4c995a5efd
bobross96 <zadus96@gmail.com>
2020-07-17 16:22:32 +0800

    test

---
+++ public/app.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+    // // The Firebase SDK is initialized and available here!
+    //
+    // firebase.auth().onAuthStateChanged(user => { });
+    // firebase.database().ref('/path/to/ref').on('value', snapshot => { });
+    // firebase.messaging().requestPermission().then(() => { });
+    // firebase.storage().ref('/path/to/ref').getDownloadURL().then(() => { });
+    //
+    // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+
+    
+      let app = firebase.app();
+   
+  });
+
+
+  function googleLogin(){
+      const provider = new firebase.auth.GoogleAuthProvider();
+      firebase.auth().signInWithPopup(provider).then(result => {
+          const user = result.user
+          document.write(`Hello ${user.displayName}`);
+          console.log(user)
+      }).catch(console.log);
+      
+  }
